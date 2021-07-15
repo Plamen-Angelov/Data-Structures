1c9dc868a1d8cd6f1cf9a2f10faa42d01802ef2d
Plamen Angelov <plamen565@gmail.com>
2021-07-15 16:23:00 +0300

    Add Problem

---
+++ Fundamentals/Linear Data Structures/DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,134 @@
+﻿namespace Problem02.DoublyLinkedList
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class DoublyLinkedList<T> : IAbstractLinkedList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(T item)
+        {
+            Node<T> node = new Node<T>();
+
+            if (Count == 0)
+            {
+                head = node;
+                tail = node;
+                node.Next = null;
+                node.Previous = null;
+            }
+            else
+            {
+                head.Previous = node;
+                node.Next = head;
+                node.Previous = null;
+                head = node;
+            }
+
+            node.Item = item;
+            Count++;
+        }
+
+        public void AddLast(T item)
+        {
+            Node<T> node = new Node<T>();
+
+            if (Count == 0)
+            {
+                head = node;
+                tail = node;
+                node.Next = null;
+                node.Previous = null;
+            }
+            else
+            {
+                tail.Next = node;
+                node.Previous = tail;
+                node.Next = null;
+                tail = node;
+            }
+
+            node.Item = item;
+            Count++;
+        }
+
+        public T GetFirst()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Collection is empty.");
+            }
+            else
+            {
+                return head.Item;
+            }
+        }
+
+        public T GetLast()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Collection is empty.");
+            }
+            else
+            {
+                return tail.Item;
+            }
+        }
+
+        public T RemoveFirst()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Collection is empty.");
+            }
+            else
+            {
+                Node<T> removed = head;
+                head.Next.Previous = null;
+                head = head.Next;
+
+                Count--;
+                return removed.Item;
+            }
+        }
+
+        public T RemoveLast()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Collection is empty.");
+            }
+            else
+            {
+                Node<T> removed = tail;
+                tail.Previous.Next = null;
+                tail = tail.Previous;
+
+                Count--;
+                return tail.Item;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> currentNode = head;
+
+            while (currentNode != null)
+            {
+                yield return currentNode.Item;
+                currentNode = currentNode.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Fundamentals/Linear Data Structures/DoublyLinkedList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Problem02.DoublyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+
+            list.AddFirst(12);
+            list.AddLast(64);
+            list.AddFirst(118);
+            list.AddLast(464);
+            Console.WriteLine(list.Count);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
