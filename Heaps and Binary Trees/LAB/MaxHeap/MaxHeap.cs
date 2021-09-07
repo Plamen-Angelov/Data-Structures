faee36cda590f98d75caec63b0c3f159c82a15cb
Plamen Angelov <plamen565@gmail.com>
2021-09-07 22:12:14 +0300

    Add to Heaps and Binary Trees

---
+++ Heaps and Binary Trees/LAB/MaxHeap/MaxHeap.cs
@@ -0,0 +1,44 @@
+﻿namespace _02.MaxHeap
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class MaxHeap<T> : IAbstractHeap<T>
+        where T : IComparable<T>
+    {
+        private List<T> heap;
+
+        public MaxHeap()
+        {
+            heap = new List<T>();
+        }
+
+        public int Size => heap.Count;
+
+        public void Add(T element)
+        {
+            heap.Add(element);
+            Heapify(Size - 1);
+        }
+
+        private void Heapify(int index)
+        {
+            if (index == 0) return;
+
+            int parentIndex = (index - 1) / 2;
+
+            if (heap[index].CompareTo(heap[parentIndex]) > 0)
+            {
+                T temp = heap[index];
+                heap[index] = heap[parentIndex];
+                heap[parentIndex] = temp;
+                Heapify(parentIndex);
+            }
+        }
+
+        public T Peek()
+        {
+            return heap[0];
+        }
+    }
+}
---
+++ Heaps and Binary Trees/LAB/MaxHeap/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.MaxHeap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var integerHeap = new MaxHeap<int>();
+            var elements = new List<int>() { 5, 6, 9, 15, 25, 8, 17, 16 };
+            foreach (var el in elements)
+            {
+                integerHeap.Add(el);
+            }
+        }
+    }
+}
